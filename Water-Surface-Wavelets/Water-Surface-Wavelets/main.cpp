c5b3b8ddb4149be7c3393ac298da45ab89371e71
Logan Dooley <logandooley123@gmail.com>
2024-07-29 19:46:45 -0400

    Project setup

---
+++ Water-Surface-Wavelets/Water-Surface-Wavelets/main.cpp
@@ -0,0 +1,23 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Window", nullptr, nullptr);
+	if (window == nullptr) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	return 0;
+}
